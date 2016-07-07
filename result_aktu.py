e276af0f05afe867542bd4e065173e90f5a4108a
Sachin <sachin@kuvera.in>
2016-07-07 10:42:51 +0530

    checks for result,on startup to be added

--- result_aktu.py
+++ result_aktu.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import subprocess
 
 page_code = urlopen("http://aktu.ac.in/results_even_15_16.html").read()
-for lines in page_code:
-	if "MCA" in lines:
-		print("found")
+soup = BeautifulSoup(page_code, 'html.parser')
+for string in soup.strings:
+    if "B.tech" in string or "B.Tech" in string or "b.tech" in string:
+        subprocess.Popen(['notify-send' , "I guess results are out :P "])
