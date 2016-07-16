bda9a7a69117eb3fe81d53b6d82935223d75a822
Sachin <sachin@kuvera.in>
2016-07-16 18:59:15 +0530

    updated

--- result_aktu.py
+++ result_aktu.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 import subprocess
 import time
 
-subprocess.Popen(['notify-send' , "I'll inform you the moment results are out :P"])
+subprocess.Popen(['notify-send' ,"I am working" ])
 while 1:
-    subprocess.Popen(['notify-send' , "I am working"])
+    subprocess.Popen(['notify-send' , "I'll inform you the moment results are out :P"])
     page_code = urlopen("http://aktu.ac.in/results_even_15_16.html").read()
     soup = BeautifulSoup(page_code,'html.parser')
-    print(soup.strings)
+    
     for string in soup.strings:
         if "B.tech Second Year" in string or "B.Tech Second Year" in string or "b.tech second year" in string:
             subprocess.Popen(['notify-send' , "I guess results are out :P "])
