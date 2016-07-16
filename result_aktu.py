7928f508d973c1780b05dfef91f02e00cf1c9e7c
Sachin <sachin@kuvera.in>
2016-07-16 12:42:33 +0530

    updated

--- result_aktu.py
+++ result_aktu.py
@@ -1,13 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import subprocess
+import time
 
 subprocess.Popen(['notify-send' , "I'll inform you the moment results are out :P"])
-while true:
-	subprocess.Popen(['notify-send' , "I am working"])
-	page_code = urlopen("http://aktu.ac.in/results_even_15_16.html").read()
-    soup = BeautifulSoup(page_code, 'html.parser')
-	for string in soup.strings:
-        if "B.tech" in string or "B.Tech" in string or "b.tech" in string:
+while 1:
+    subprocess.Popen(['notify-send' , "I am working"])
+    page_code = urlopen("http://aktu.ac.in/results_even_15_16.html").read()
+    soup = BeautifulSoup(page_code,'html.parser')
+    print(soup.strings)
+    for string in soup.strings:
+        if "B.tech Second Year" in string or "B.Tech Second Year" in string or "b.tech second year" in string:
             subprocess.Popen(['notify-send' , "I guess results are out :P "])
     time.sleep(600)
